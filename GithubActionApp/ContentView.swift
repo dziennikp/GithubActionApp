504f2dd754dab54270609565123f7bba02e1969b
Paweł Dziennik <dziennikp@gmail.com>
2020-04-24 13:54:37 +0200

    Initial Commit

---
+++ GithubActionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubActionApp
+//
+//  Created by Paweł Dziennik on 24/04/2020.
+//  Copyright © 2020 Crux Solutions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubActionAppTests/GithubActionAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  GithubActionAppTests.swift
+//  GithubActionAppTests
+//
+//  Created by Paweł Dziennik on 24/04/2020.
+//  Copyright © 2020 Crux Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import GithubActionApp
+
+class GithubActionAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
